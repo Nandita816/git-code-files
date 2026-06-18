a32f2387eba5a87753e830d457eb54b6af126364
Nandita <metinandu.23@gmail.com>
2026-06-18 16:55:28 +0530

    exception handling and pydantic

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-print("simple first line")
-print("simple second line")
-print("simple third line")
 
-def funct1():
-    return none
+from pydantic import BaseModel, Field
+from typing import Annotated
+
+class CourseRegistration(BaseModel):
+    # Username must be between 3 and 15 characters
+    username: Annotated[str, Field(min_length=3, max_length=15, title = "name of the user",description="Give the name of the user in 15 characters", examples=["jackob", "john"])]
     
-def function_for_barnch():
-    return error
+    # Age must be at least 18
+    age: Annotated[int, Field(gt=17, title="Student Age")] 
+
+# If we try to use a 2-letter username, Pydantic will raise a validation error
+try:
+    user = CourseRegistration(username="Nadeem",age = 18)
+except Exception as e:
+    print(f"Validation failed: {e}")
+else:
+    print("user registered")
---
+++ package1/module1.py
@@ -0,0 +1,6 @@
+def module1_func1():
+    print("This is function 1 from module1")
+
+
+def func2():
+    print("This is function 2 from module1")
